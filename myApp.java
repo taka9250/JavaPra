adb3032ea9c4a31b7597ddae31e89f465eca0f9a
taka9250 <takahiro92502520@gmail.com>
2016-08-21 17:22:22 +0900

    二つの整数の大小判定

--- myApp.java
+++ myApp.java
@@ -7,18 +7,23 @@ import java.io.InputStreamReader;
 public class myApp {
 
 	public static void main(String[] args) throws IOException {
-		System.out.println("整数を入力してください。");
+
+		System.out.println("二つ整数を入力してください。");
+
 		BufferedReader br =
 				new BufferedReader(new InputStreamReader(System.in));
 
-		String str = br.readLine();
-		int res = Integer.parseInt(str);
+		String str1 = br.readLine();
+		String str2 = br.readLine();
+		int num1 = Integer.parseInt(str1);
+		int num2 = Integer.parseInt(str2);
 
-		int ans = res % 2;
-		if(ans == 0){
-			System.out.println(res + "は偶数です。");
+		if(num1 == num2){
+			System.out.println(num1 + "と" + num2 +"は同じ数です。");
+		}else if(num1 > num2){
+			System.out.println(num1 + "は" + num2 +  "より大きいです。");
 		}else{
-			System.out.println(res + "は奇数です。");
+			System.out.println(num1 + "は" + num2 +  "より小さいです。");
 		}
 	}
 
